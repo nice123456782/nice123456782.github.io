a807e2123c4748997697ef102502122454f4792d
nice123456782 <102738586+nice123456782@users.noreply.github.com>
2022-03-31 16:02:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 <h1>Alben <input id="search" type="text" placeholder="Search"/></h1>
+$(document).ready(function() {
+$("#search").keyup(function() {
+});
+});
+var albums = $(".album");
+$(albums).show();
+var searchTerm = $(this).val().toLocaleLowerCase()
+albums.each(function(){
+if($(this).text().toLocaleLowerCase().search(searchTerm) == -1) {
+$(this).hide();
+}
+});
